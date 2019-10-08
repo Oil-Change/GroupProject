079bf7e8b496ca0601406f794b71ef13c6e23b66
Matthew Kastner <matthewkastner@gmail.com>
2019-10-08 13:43:03 -0600

    routes implemented

---
+++ src/routes/router.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+
+//Import components
+import LandingPage from '../components/landingPage/LandingPage';
+import InstructionsPage from '../components/instructionsPage/InstructionsPage'
+import Login from '../components/login/Login'
+import Register from '../components/register/Register';
+import Stripe from '../components/stripe/Stripe'
+import Messages from '../components/message/Message'
+
+//Router
+export default (
+    <Switch>
+        <Route exact path="/" component={LandingPage}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register}/>
+        <Route path="/car" component={SignUp}/>
+        <Route path="/payment" component={Stripe}/>
+        <Route path="/instructions" component={InstructionsPage}/>
+        <Route path="/messages" component={Messages}/>
+    </Switch>
+)
