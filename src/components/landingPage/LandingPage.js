fbd511f5bd2bd7e5e390699938468885cd5d0e94
Chris Juber <cjuber5@gmail.com>
2019-10-10 14:25:26 -0600

    styling

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -3,19 +3,21 @@ import React, { Component } from 'react'
 export default class LandingPage extends Component {
     render() {
 
-        const img = require('../../assets/landingPage2.png')
+        const img = require('../../assets/landingPage3.png')
         return (
             <div className="landing-container">
-                <div>
+                <div className="top-section">
                 <h1 className="welcome">WELCOME</h1>
-                <p className="greeting-text">Let's get that pesky oil change taken care of!</p>
-                </div>
                 <div className="image-container">
                     <img src={img} alt="oil-change"/>
-                    <span></span>
                 </div>
+                </div>
+                <div className="bot-section">
+                
+                <p className="greeting-text">Let's get that pesky oil change taken care of!</p>
                 <button onClick={() => this.props.history.push(`/login`)}
                 className="next-btn">LET'S GO!</button>
+                </div>
             </div>
         )
     }
