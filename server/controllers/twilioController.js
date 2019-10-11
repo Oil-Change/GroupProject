2341135fa1c6973e487bfecae07871e0ac279a6f
Chris Juber <cjuber5@gmail.com>
2019-10-11 10:40:00 -0600

    stuff

--- server/controllers/twilioController.js
+++ server/controllers/twilioController.js
@@ -12,13 +12,14 @@ const sendCode = (req, res) => {
 
     //_GET Variables
     const { phone_number, code } = req.body;
-
+    console.log(phone_number, code)
     //Send Text
     client.messages.create({
         body: `Your verification code is ${code}`,
         to: phone_number,  // Text this number
         from: '+18582174901' // From a valid Twilio number
     }).then((message) => console.log(message.body))
+    res.status(200).send('sent')
 }
 
 const pickUp = (req, res) => {
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,6 +12,13 @@ module.exports = {
     createUser: (req, res) => {
         const { phone_number } = req.body
 
+        console.log("verifyCode", phone_number)
+
+        const db = req.app.get('db')
+        const foundUser = await db.user.get_user([phone_number])
+        if(foundUser[0]){
+            res.status(409).send('exists')
+        }
         const db = req.app.get('db')
         db.user.create_user([phone_number])
             .then((response) => res.status(200).send(response))
@@ -21,6 +28,7 @@ module.exports = {
             })
         req.session.user = { phone_number: phone_number }
     },
+
     updateUser: (req, res) => {
         const { firstName, lastName, street, city, state, zip, email } = req.body
         const phone_number = '4352325367'
@@ -38,14 +46,9 @@ module.exports = {
     },
     updateCode: async (req, res) => {
         const { code, phone_number } = req.body
+        console.log(code, phone_number)
         const db = req.app.get('db')
-        await db.user.update_user_code([phone_number, code])
+        await db.user.update_code([phone_number, code])
         return res.status(200).send("set")
-    },
-    removeCode: async (req, res) => {
-        const { phone_number } = req.body
-        const db = req.app.get('db')
-        await db.user.remove_user_code([phone_number])
-        return res.status(200).send("cleared")
     }
 };
